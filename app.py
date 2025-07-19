a11ba926f9dbd6e82937666c7fb767af00d2124e
Aditya Giri <adityagiri206@gmail.com>
2025-07-20 00:43:17 +0700

    fix: add host and port for Railway

--- app.py
+++ app.py
@@ -410,4 +410,4 @@ def admin_delete_tiket(ticket_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
